09f617026181156df5b13742139ec48e76a03e0b
= <=>
2026-06-16 17:30:29 -0500

    Fin Laboratorio7

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,7 +6,7 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -16,7 +15,25 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Tab 3</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        
+        
+        <div className="card-container">
+          <IonCard className='card'>
+            <img
+            src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+            alt='Avatar'
+            />
+            <IonHeader>
+              <IonCardTitle>Daniela Pinto</IonCardTitle>
+              <IonCardSubtitle>danielapintobazante</IonCardSubtitle>
+              <IonCardContent>
+                <p>Desarrollador de software con experiencia en aplicaciones moviles y web. Apasionado por la tecnologia, el aprendizaj conrinuo</p>
+              </IonCardContent>
+            </IonHeader>
+          </IonCard>
+        </div>
+        
+
       </IonContent>
     </IonPage>
   );
